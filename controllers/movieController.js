336c244fda13296b21b4f63d1f4e888720f64407
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-04-15 15:19:04 +0200

    add controllers

---
+++ controllers/movieController.js
@@ -0,0 +1,45 @@
+const connection = require('../data/db')
+
+
+function index(req, res) {
+    const sql = 'SELECT * FROM movies'
+
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err })
+        console.log(results)
+        res.json(results)
+    })
+}
+
+function show(req, res) {
+    const id = req.params.id
+    const sql = `SELECT * FROM movies_db.movies WHERE movies.id = ?`
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: err })
+        console.log(results)
+        if (results.length == 0) return res.status(404).json({ error: '404 not found' })
+        res.json(results)
+    })
+}
+
+function store(req, res) {
+}
+
+function update(req, res) {
+}
+
+function modify(req, res) {
+}
+
+function destroy(req, res) {
+}
+
+module.exports = {
+    index,
+    show,
+    store,
+    update,
+    modify,
+    destroy
+}
--- routes/movies.js
+++ routes/movies.js
@@ -1,44 +1,18 @@
 const express = require('express')
 const router = express.Router();
-const connection = require('../data/db')
+const moviesController = require("../controllers/movieController")
 
 //index
-router.get('/', (req, res) => {
-    const sql = 'SELECT * FROM movies'
-
-    connection.query(sql, (err, results) => {
-        if (err) return res.status(500).json({ error: err })
-        console.log(results)
-        res.json(results)
-    })
-})
+router.get('/', moviesController.index)
 //show
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    const sql = `SELECT * FROM movies_db.movies WHERE movies.id = ?`
-
-    connection.query(sql, [id], (err, results) => {
-        if (err) return res.status(500).json({ error: err })
-        console.log(results)
-        if (results.length == 0) return res.status(404).json({ error: '404 not found' })
-        res.json(results)
-    })
-})
+router.get('/:id', moviesController.show)
 //store
-router.post('/:id', (req, res) => {
-
-})
+router.post('/:id', moviesController.store)
 //update
-router.put('/:id', (req, res) => {
-
-})
+router.put('/:id', moviesController.update)
 //partial update
-router.patch('/:id', (req, res) => {
-
-})
+router.patch('/:id', moviesController.modify)
 //delete
-router.delete('/:id', (req, res) => {
-
-})
+router.delete('/:id', moviesController.destroy)
 
 module.exports = router;
