4e0396c0db077a71b4c6327f0b1eb73af826d795
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-04-15 15:21:54 +0200

    add cors rules

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express()
 const port = 3000
-
+const cors = require("cors");
 const moviesRouter = require('./routes/movies');
 
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 app.listen(port, () => {
     console.log(`server running on http://localhost:${port}`)
