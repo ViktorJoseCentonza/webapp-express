decb3f94e98f0cca05288c4ad7509e6fb30f3cd2
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-04-15 18:47:34 +0200

    fix show route not returning reviews

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -13,7 +13,8 @@ function index(req, res) {
 
 function show(req, res) {
     const id = req.params.id
-    const sql = `SELECT * FROM movies_db.movies WHERE movies.id = ?`
+    const sql = `SELECT movies.id,movies.title,movies.director,movies.genre,movies.release_year,movies.image,JSON_ARRAYAGG(JSON_OBJECT('name', reviews.name,'vote', reviews.vote,'text', reviews.text,'created_at', reviews.created_at,'updated_at', reviews.updated_at)) AS reviews FROM movies LEFT JOIN reviews ON reviews.movie_id = movies.id WHERE movies.id = ? GROUP BY movies.id;
+`
 
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: err })
