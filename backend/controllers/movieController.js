12441c7f8f71a3e8a36c2307510d024da1b53b35
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-04-16 14:35:21 +0200

    add average rating to show route

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -8,12 +8,13 @@ function index(req, res) {
         if (err) return res.status(500).json({ error: err })
         console.log(results)
         res.json(results)
+        console.log("index route used!")
     })
 }
 
 function show(req, res) {
     const id = req.params.id
-    const sql = `SELECT movies.id,movies.title,movies.director,movies.genre,movies.release_year,movies.image,JSON_ARRAYAGG(JSON_OBJECT('name', reviews.name,'vote', reviews.vote,'text', reviews.text,'created_at', reviews.created_at,'updated_at', reviews.updated_at)) AS reviews FROM movies LEFT JOIN reviews ON reviews.movie_id = movies.id WHERE movies.id = ? GROUP BY movies.id;
+    const sql = `SELECT movies.id,movies.title,movies.director,movies.genre,movies.release_year,movies.abstract, movies.image, AVG(vote) as average_rating,JSON_ARRAYAGG(JSON_OBJECT('name', reviews.name,'vote', reviews.vote,'text', reviews.text,'created_at', reviews.created_at,'updated_at', reviews.updated_at)) AS reviews FROM movies LEFT JOIN reviews ON reviews.movie_id = movies.id WHERE movies.id = ? GROUP BY movies.id;
 `
 
     connection.query(sql, [id], (err, results) => {
@@ -25,7 +26,7 @@ function show(req, res) {
                 message: "elemento non trovato"
             });
         }
-
+        console.log("show route used!")
         res.json(results)
     })
 }
